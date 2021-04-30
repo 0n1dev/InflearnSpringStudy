16a3b33c5d42c9475b22d24a5c5db5b6c3fab674
0n1dev <all_mind@naver.com>
2021-04-30 17:35:49 +0900

    [#5] Java 코드를 사용한 스프링 빈 등록 (컴포넌트 스캔)

--- src/main/java/kr/spring/springapplicationcontext/BookRepository.java
+++ src/main/java/kr/spring/springapplicationcontext/BookRepository.java
@@ -1,4 +1,7 @@
 package kr.spring.springapplicationcontext;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class BookRepository {
 }
--- src/main/java/kr/spring/springapplicationcontext/BookService.java
+++ src/main/java/kr/spring/springapplicationcontext/BookService.java
@@ -1,9 +1,12 @@
 package kr.spring.springapplicationcontext;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class BookService {
 
+    @Autowired
     BookRepository bookRepository;
 
     public void setBookRepository(BookRepository bookRepository) {
