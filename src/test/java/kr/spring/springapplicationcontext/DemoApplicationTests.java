229a57e67ed0e986e490171ae98b7fa4edd577b6
0n1dev <all_mind@naver.com>
2021-04-30 17:06:42 +0900

    [#1] Init Spring Project

---
+++ src/test/java/kr/spring/springapplicationcontext/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.spring.springapplicationcontext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
