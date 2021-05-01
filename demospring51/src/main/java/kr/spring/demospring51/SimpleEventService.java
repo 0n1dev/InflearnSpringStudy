6adcc774abef7f0a1947b6237d5666fa4544ea61
0n1dev <all_mind@naver.com>
2021-05-01 18:44:49 +0900

    [#28] AOP - 프록시 패턴 적용 후

--- demospring51/src/main/java/kr/spring/demospring51/SimpleEventService.java
+++ demospring51/src/main/java/kr/spring/demospring51/SimpleEventService.java
@@ -6,30 +6,22 @@ import org.springframework.stereotype.Service;
 public class SimpleEventService implements EventService {
     @Override
     public void createEvent() {
-        long begin = System.currentTimeMillis();
-
         try {
             Thread.sleep(1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
         System.out.println("Created");
-        System.out.println(System.currentTimeMillis() - begin);
     }
 
     @Override
     public void publishEvent() {
-        long begin = System.currentTimeMillis();
-
         try {
             Thread.sleep(2000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
         System.out.println("Publish");
-        System.out.println(System.currentTimeMillis() - begin);
     }
 
     @Override
