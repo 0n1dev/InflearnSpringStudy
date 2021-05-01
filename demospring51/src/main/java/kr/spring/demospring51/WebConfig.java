08a0317df8b9cca470ee28cfc46aaac9568eb562
0n1dev <all_mind@naver.com>
2021-05-01 17:17:15 +0900

    [#24] 데이터 바인딩 추상화 - ConversionService - Spring Framework에서는 WebMvcConfigurer의 메서드를 통해 ConversionService에 등록해야함 - ConversionService에서는 실제로 데이터 변환이 일어남 - 데이터 바인딩도 일어남

--- demospring51/src/main/java/kr/spring/demospring51/WebConfig.java
+++ demospring51/src/main/java/kr/spring/demospring51/WebConfig.java
@@ -4,12 +4,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+@Configuration // Spring Framework에서는 WebMvcConfigurer을 통해 ConversionService에 등록해야함
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addFormatters(FormatterRegistry registry) {
-//        registry.addConverter(new EventConverter.StringToEventConverter());
-        registry.addFormatter(new EventFormatter());
+//        registry.addConverter(new EventConverter.StringToEventConverter()); // 컨버터 등록
+        registry.addFormatter(new EventFormatter()); // 포매터 등록
     }
 }
