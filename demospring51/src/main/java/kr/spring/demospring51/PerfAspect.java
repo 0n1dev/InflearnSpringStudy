06ad78f30f750717b5768f1a1e2889e7343a2837
0n1dev <all_mind@naver.com>
2021-05-01 18:57:37 +0900

    [#30] AOP - 어노테이션 기반 @Before 추가

--- demospring51/src/main/java/kr/spring/demospring51/PerfAspect.java
+++ demospring51/src/main/java/kr/spring/demospring51/PerfAspect.java
@@ -3,6 +3,7 @@ package kr.spring.demospring51;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -15,5 +16,10 @@ public class PerfAspect {
         Object retVal = jp.proceed();
         System.out.println(System.currentTimeMillis() - begin);
         return retVal;
+    }
+
+    @Before("bean(simpleEventService)") // 해당 빈의 모든 메서드 실행 이전
+    public void hello() {
+        System.out.println("=====================");
     }
 }
